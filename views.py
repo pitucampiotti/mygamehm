9813a3391aab92b0a798a9a4c278cedb36cf3bfa
pitucampiotti <pitucampiotti@gmail.com>
2022-07-26 17:30:00 -0300

    Adding rules page

--- views.py
+++ views.py
@@ -5,3 +5,8 @@ views = Blueprint(__name__, "views")
 @views.route("/")
 def home ():
     return render_template("index.html", card=getcard())
+
+
+@views.route("/rules")
+def rules():
+    return  render_template("rules.html")
